79b30f343fd460f3e284c793c8f4535c1dfb034c
torjusljung <tolj22ab@student.cbs.dk>
2023-09-28 19:01:37 +0200

    up

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(express.static(path.join(__dirname, "../client")))
+app.use(express.static(path.join(__dirname, "client")))
 
 
 app.get("/", (req, res) => {
