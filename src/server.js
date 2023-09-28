8ae85ce599103f6f92235172a1564a0cf6db57af
torjusljung <tolj22ab@student.cbs.dk>
2023-09-28 19:09:48 +0200

    hh

--- src/server.js
+++ src/server.js
@@ -13,19 +13,19 @@ app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(express.static(path.join(__dirname, "../client")))
+app.use(express.static(path.join(__dirname, ".../client")))
 
 
-app.get("/home.html", (req, res) => {
-  res.sendFile(path.join(__dirname + "../client/pages/home.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + ".../client/pages/home.html"));
 })
 
 app.get("/home.js", (req, res) => {
-  res.sendFile(path.join(__dirname + "../client/scripts/home.js"));
+  res.sendFile(path.join(__dirname + ".../client/scripts/home.js"));
 })
 
 app.get("/global.css", (req, res) => {
-  res.sendFile(path.join(__dirname + "../client/styles/global.css"));
+  res.sendFile(path.join(__dirname + ".../client/styles/global.css"));
 })
 
 .post('/', (req, res) => {
