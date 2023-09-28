031c1206b79dc1257d713945262d5a3d5d16470d
torjusljung <tolj22ab@student.cbs.dk>
2023-09-28 19:02:25 +0200

    up

--- src/server.js
+++ src/server.js
@@ -17,15 +17,15 @@ app.use(express.static(path.join(__dirname, "client")))
 
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "../client/pages/home.html"));
+  res.sendFile(path.join(__dirname + "client/pages/home.html"));
 })
 
 app.get("/home.js", (req, res) => {
-  res.sendFile(path.join(__dirname + "../client/scripts/home.js"));
+  res.sendFile(path.join(__dirname + "client/scripts/home.js"));
 })
 
 app.get("/global.css", (req, res) => {
-  res.sendFile(path.join(__dirname + "../client/styles/global.css"));
+  res.sendFile(path.join(__dirname + "client/styles/global.css"));
 })
 
 .post('/', (req, res) => {
